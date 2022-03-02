67154011071e7145f3ca6c96d93df976829148dd
Lee Eunji <lej970703@naver.com>
2022-03-03 00:48:53 +0900

    :lipstick: Create Double Entry Bookkeeping

--- src/components/history/DoubleEntryTable.js
+++ src/components/history/DoubleEntryTable.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import TableContainer from '@mui/material/TableContainer';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableRow from '@mui/material/TableRow';
-import TableHead from '@mui/material/TableHead';
-import TableCell from '@mui/material/TableCell';
-import { TableBody } from '@mui/material/index';
+import { TableBody, TableCell, TableHead, TableRow, Table, Paper } from '@mui/material/index';
+import { faArrowUp, faArrowDown} from '@fortawesome/free-solid-svg-icons/index';
+import { FontAwesomeIcon } from '../../../node_modules/@fortawesome/react-fontawesome/index';
+
 const TableBlock = styled.div`
   width: 100%;
   height: 40rem;
@@ -16,11 +14,113 @@ const TableBlock = styled.div`
     thead {
       background-color: #f3f6f9;
     }
+    .plus {
+      margin-left: 0.5rem;
+      color: red;
+    }
+    .minus {
+      margin-left: 0.5rem;
+      color: blue;
+    }
   }
 `;
 
 const upperColumns = ['일자', '카테고리', '항목', '차변', '우변', '메모'];
-
+const list = [
+  // 계좌 입금
+  {
+    date: '2022-03-02',
+    category:'용돈',
+    content: '저금통 깼음',
+    debtAccount: '자산(예금)',
+    debtSign: 1,
+    amount: 3000,
+    creditAccount: '자산(현금)',
+    creditSign: -1,
+    memo: '설거지 값'
+  },
+  // 계좌 인출
+  {
+    date: '2022-03-02',
+    category: '기타',
+    content: '현금화',
+    debtAccount: '자산(예금)',
+    debtSign: -1,
+    amount: 10000,
+    creditAccount: '자산(현금)',
+    creditSign: 1
+  },
+  // 현금성 수입
+  {
+    date: '2022-03-02',
+    category: '용돈',
+    content: '2월 용돈',
+    debtAccount: '자산(현금)',
+    debtSign: 1,
+    amount: 3000,
+    creditAccount: '수익',
+    creditSign: 0
+  },
+  // 예금성 수입
+  {
+    date: '2022-03-02',
+    category: '용돈',
+    content: '1월 이자',
+    debtAccount: '자산(예금)',
+    debtSign: 1,
+    amount: 3000,
+    creditAccount: '수익',
+    creditSign: 0
+  },
+  // 현금성 지출
+  {
+    date: '2022-03-02',
+    category: '식비',
+    content: '순이네 마트',
+    debtAccount: '비용',
+    debtSign:0,
+    amount: 1500,
+    creditAccount: '자산(현금)',
+    creditSign: -1,
+    memo: '과자'
+  },
+  // 예금성 지출
+  {
+    date: '2022-03-02',
+    category: '용돈',
+    content: '순이네 마트',
+    debtAccount: '비용',
+    debtSign:0,
+    amount: 4500,
+    creditAccount: '자산(예금)',
+    creditSign: -1,
+    memo: '고무장갑'
+  },
+  // 신용카드 사용
+  {
+    date: '2022-03-02',
+    category: '전자기기',
+    content: '하이마트',
+    debtAccount: '비용',
+    debtSign: 0,
+    amount: 50000,
+    creditAccount: '부채',
+    creditSign: 1,
+    memo: '이어폰'
+  },
+  // 신용카드 대금 납부
+  {
+    date: '2022-03-02',
+    category: '대금 납부',
+    content: '우리카드결제',
+    debtAccount: '부채',
+    debtSign: -1,
+    amount: 50000,
+    creditAccount: '자산(예금)',
+    creditSign: -1,
+    memo: '이어폰'
+  }
+];
 const DoubleEntryTable = () => {
   return (
     <TableBlock>
@@ -48,16 +148,24 @@ const DoubleEntryTable = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-            </TableRow>
+            {list.map((h, idx) => (
+              <TableRow key={idx} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell>{h.date}</TableCell>
+                <TableCell>{h.category}</TableCell>
+                <TableCell>{h.content}</TableCell>
+                <TableCell>
+                  {h.debtAccount}
+                  {h.debtSign == -1 ? <FontAwesomeIcon className="minus" icon={faArrowDown}/> : h.debtSign == 1 ? <FontAwesomeIcon className="plus" icon={faArrowUp}/> : ''}
+                </TableCell>
+                <TableCell>{h.amount}</TableCell>
+                <TableCell>
+                  {h.creditAccount}
+                  {h.creditSign == -1 ? <FontAwesomeIcon className="minus" icon={faArrowDown}/> : h.creditSign == 1 ? <FontAwesomeIcon className="plus" icon={faArrowUp}/> : ''}
+                </TableCell>
+                <TableCell>{h.amount}</TableCell>
+                <TableCell>{h.memo}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
